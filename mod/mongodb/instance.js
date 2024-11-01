80690c43695d7f6eaf6182ad369cab5ac1fe54b4
minhla <anthonyle9x@gmail.com>
2024-11-01 12:28:44 +0700

    Export collection connect instance instead of database instance

--- mod/mongodb/instance.js
+++ mod/mongodb/instance.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { MongoClient } from "mongodb";
 
-const { CONNECT_USERNAME, CONNECT_PASSWORD, ATLAS_DATABASE_URL, DATABASE_NAME } = process.env;
+const { CONNECT_USERNAME, CONNECT_PASSWORD, ATLAS_DATABASE_URL, DATABASE_NAME, COLLECTION_NAME } = process.env;
 
 const uri = `mongodb+srv://${CONNECT_USERNAME}:${CONNECT_PASSWORD}@${ATLAS_DATABASE_URL}/`;
 
@@ -8,4 +10,6 @@ const client = new MongoClient(uri);
 
 const database = client.db(DATABASE_NAME);
 
-export default database;
+const collection = database.collection(COLLECTION_NAME);
+
+export default collection;
