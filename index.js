e40f713ef407e9db042c136b85789f8f94f3f46c
minhla <anthonyle9x@gmail.com>
2024-11-01 12:59:49 +0700

    Update default GET route and add error handler for non-existent routes

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 
 app.use("/api/courses", coursesRouter);
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Edunity Backend API Server");
+});
+
+// Error handler for routes that don't exist
+app.use((req, res) => {
   res.status(404).send({ message: "Not Found" });
 });
 
