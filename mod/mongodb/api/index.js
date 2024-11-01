0cce881496fb4230ed2bc22ca7507a4fce33261e
minhla <anthonyle9x@gmail.com>
2024-11-01 19:22:58 +0700

    Add dev endpoint "Add Courses" to courses router

--- mod/mongodb/api/index.js
+++ mod/mongodb/api/index.js
@@ -3,10 +3,17 @@ import express from "express";
 import deleteCourse from "./delete.js";
 import createCourse from "./create.js";
 import searchCourses from "./search.js";
+import { addCourses } from "./add.js";
+
+const isDev = process.env.NODE_ENV === "development";
 
 const coursesRouter = express.Router();
 
 coursesRouter.use(express.json());
+
+if (isDev) {
+  coursesRouter.post("/add", addCourses);
+}
 
 coursesRouter.get("/search", searchCourses);
 
